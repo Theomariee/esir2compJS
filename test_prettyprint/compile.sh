9e75cf68036e813304030d955ac38627ba86700c
Corentin Duchatelet <corentin.duchatelet@orange.fr>
2018-11-18 23:38:00 +0100

    Add files via upload

--- test_prettyprint/compile.sh
+++ test_prettyprint/compile.sh
@@ -3,6 +3,7 @@
 
 #Empty the output folder before running the generation of files
 rm -rf compiled/*
+rm ./toCompile/test9.wh
 
 for filename in `ls toCompile`
 do
@@ -10,42 +11,8 @@ do
 	./whpp ./toCompile/$filename -o ./compiled/$filename
 done
 
-# #multiplier le fichier 1 x fois pour les tests de linéarité
-
-# #un fichier de 12 lignes, 3 fois le fichier 1
-# rm ./toCompile/test9.wh
-# for i in `seq 1 3`
-# do
-# 	./toCompile/test1.wh >> ./toCompile/test9.wh
-# done
-# START=$(date +%s.%N)
-# java -jar ./toCompile/test9.wh ./compiled/test9.wh
-# END=$(date +%s.%N)
-# DIFF=$(echo "$END - $START" | bc)
-# DIFF > ./compiled/test9time.txt
-
-
-# #un fichier de 120 lignes, 30 fois le fichier 1
-# rm ./toCompile/test10.wh
-# for i in `seq 1 3`
-# do
-# 	./toCompile/test1.wh >> ./toCompile/test10.wh
-# done
-# START=$(date +%s.%N)
-# java -jar ./toCompile/test10.wh ./compiled/test10.wh
-# END=$(date +%s.%N)
-# DIFF=$(echo "$END - $START" | bc)
-# DIFF > ./compiled/test10time.txt
-
-
-# #un fichier de 1200 lignes, 300 fois le fichier 1
-# rm ./toCompile/test11.wh
-# for i in `seq 1 3`
-# do
-# 	./toCompile/test1.wh >> ./toCompile/test11.wh
-# done
-# START=$(date +%s.%N)
-# java -jar ./toCompile/test11.wh ./compiled/test11.wh
-# END=$(date +%s.%N)
-# DIFF=$(echo "$END - $START" | bc)
-# DIFF > ./compiled/test11time.txt
+#pretty-print du pretty-print
+filename="test9"
+cp ./compiled/test1.wh ./expectedCompilation/expectedTest9.wh
+cp ./compiled/test1.wh ./toCompile/$filename.wh
+./whpp ./toCompile/$filename.wh -o ./compiled/$filename.wh
