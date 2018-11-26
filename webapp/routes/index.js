ee0a8a95174fdade1932606b86dbabd0ef4df624
Corentin Lefranc <lefranc.corentin@hotmail.fr>
2018-11-26 23:25:21 +0100

    Updated test webapp again

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 
 /*router.get('/', (req, res) => {
   res.send('It works!');
@@ -7,15 +8,26 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
 	res.render('index');
+	var path = require('path');
+	var appDir = path.dirname(require.main.filename);
 });
 
 
 router.post('/test', (req, res) => {
+	var whilePrgm = req.body.whilePrgm;
+	fs.writeFile("input.wh", whilePrgm, function(err) {
+	    if(err) {
+	        return console.log(err);
+	    }
+	    console.log("WHILE program saved!");
+	});
+
 	console.log('Executing jar');
 	var exec = require('child_process').exec;
-	exec('java -jar testjar.jar', function (error, stdout, stderr){
+	exec('java -jar ../livrables/whc1/whc.jar input.wh', function (error, stdout, stderr){
 		console.log('Sending jar output to client');
-		res.json({'data': stdout})
+		console.log(stdout);
+		res.json({'data': stdout});
 		if(error !== null){
 			console.log("Error -> "+error);
 			res.json({'data': error})
