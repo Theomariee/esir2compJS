9d7723130c6377a0bad6d96a39eb7317e8cfb629
Corentin Lefranc <lefranc.corentin@hotmail.fr>
2018-11-27 23:41:54 +0100

    Split prints Output/Console

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -1,19 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
-
-/*router.get('/', (req, res) => {
-  res.send('It works!');
-});*/
+var exec = require('child_process').exec;
 
 router.get('/', (req, res) => {
 	res.render('index');
-	var path = require('path');
-	var appDir = path.dirname(require.main.filename);
 });
 
-
-router.post('/test', (req, res) => {
+router.post('/compile', (req, res) => {
 	var whilePrgm = req.body.whilePrgm;
 	fs.writeFile("input.wh", whilePrgm, function(err) {
 	    if(err) {
@@ -22,16 +16,28 @@ router.post('/test', (req, res) => {
 	    console.log("WHILE program saved!");
 	});
 
+	// delete compiled.js if it exists
+	if (fs.existsSync('compiled.js')) {
+    	fs.unlinkSync('compiled.js'); 
+	}
+	
 	console.log('Executing jar');
-	var exec = require('child_process').exec;
-	exec('java -jar ../livrables/whc1/whc.jar input.wh', function (error, stdout, stderr){
+	exec('java -jar ../livrables/whc1/whc.jar input.wh -o compiled.js', function (error, stdout, stderr){
 		console.log('Sending jar file output to client');	
 		console.log(stdout);
-		res.json({'data': stdout});
+		res.json({'jarPrints': stdout});
 		if(error !== null){
 			console.log("Error -> "+error);
 			res.json({'data': error})
 		}
+	});
+});
+
+router.get('/output', (req, res) => {
+	fs.readFile("compiled.js", 'utf-8', function (err, data) {
+	    if (err) throw err;
+	    console.log('Sending JS output to client');
+	    res.json({'compiledJS': data});
 	});
 });
 
