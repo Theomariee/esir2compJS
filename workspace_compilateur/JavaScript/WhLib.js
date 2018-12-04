1cb40d6eeffa17ed074146ac03468c1ff8436ad6
Corentin Duchatelet <corentin.duchatelet@orange.fr>
2018-12-04 11:56:27 +0100

    Update WhLib.js
    controlling addr3 argument

--- workspace_compilateur/JavaScript/WhLib.js
+++ workspace_compilateur/JavaScript/WhLib.js
@@ -11,7 +11,9 @@ function nil(){
 function cons(addr2,addr3){
     var args=[];
     args.push(Object.assign(Object.create(Object.getPrototypeOf(addr2)),addr2)); //clone the addr2
-    args.push(Object.assign(Object.create(Object.getPrototypeOf(addr3)),addr3)); //clone the addr3
+    if(addr3) {
+        args.push(Object.assign(Object.create(Object.getPrototypeOf(addr3)),addr3)); //clone the addr3
+    }
     return bt.cons(args); //return the new bintree
 }
 
@@ -19,16 +21,18 @@ function cons(addr2,addr3){
 function list(addr2,addr3){
     var args=[];
     args.push(Object.assign(Object.create(Object.getPrototypeOf(addr2)),addr2)); //clone the addr2
-    args.push(Object.assign(Object.create(Object.getPrototypeOf(addr3)),addr3)); //clone the addr3
+    if(addr3) {
+        args.push(Object.assign(Object.create(Object.getPrototypeOf(addr3)),addr3)); //clone the addr3
+    }
     return bt.list(args); //return the new bintree
 }
 
 function hd(addr2){
-    return bt.head(addr2);
+    return bt.head(Object.assign(Object.create(Object.getPrototypeOf(addr2)),addr2));
 }
 
 function tl(addr2){
-    return bt.tail(addr2);
+    return bt.tail(Object.assign(Object.create(Object.getPrototypeOf(addr2)),addr2));
 }
 
 function not(addr2){
@@ -40,7 +44,7 @@ var a = new BinTree("nil",null,null);
 var b = new BinTree("nil",null,null);
 var res = cons(a,b);
 console.log(res);
-var a = new BinTree("cons",null,null);
+a = new BinTree("cons",null,null);
 console.log(res);
 console.log(hd(res));
 console.log(not(b));
