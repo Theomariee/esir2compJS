60eff1351844b510f1cdb1de9f62a9822ca02524
Corentin Lefranc <lefranc.corentin@hotmail.fr>
2018-12-05 17:53:45 +0100

    Added dynamic function parameter inputs

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -3,10 +3,19 @@ const router = express.Router();
 const fs = require('fs');
 var exec = require('child_process').exec;
 
+// render la page
 router.get('/', (req, res) => {
 	res.render('index');
+	if (fs.existsSync('temp/main.txt')) {
+		fs.readFile("temp/main.txt", 'utf-8', function (err, data) {
+		    if (err) throw err;
+		    var test = data.split(',').length;
+		    console.log(test);
+		});
+	}
 });
 
+//appelé en ajax, exécute le jar et affiche renvoie ses sorties standard et erreur pour afficher dans la "console" 
 router.post('/compile', (req, res) => {
 	var whilePrgm = req.body.whilePrgm;
 	fs.writeFile("temp/input.wh", whilePrgm, function(err) {
@@ -33,18 +42,42 @@ router.post('/compile', (req, res) => {
 	});
 });
 
+// appelé en ajax si la requête /compile est successful (cad le fichier compiled.js a dû être généré)
 router.get('/output', (req, res) => {
 	if (fs.existsSync('temp/compiled.js')) {
 		fs.readFile("temp/compiled.js", 'utf-8', function (err, data) {
-		    if (err) throw err;
-		    console.log('Sending JS output to client');
-		    res.json({'compiledJS': data});
+		    if (err) {
+		    	throw err;
+		    }else{
+			    console.log('Sending JS output to client');
+			    res.json({'compiledJS': data});		    	
+		    }
 		});
 	}else{
 		var error = 'temp/compiled.js not found';
 		res.json({'compiledJS': error});
 	}
 });
+
+// récupère le nombre de paramètres via le fichier txt généré par le jar
+router.get('/parameters', (req, res) => {
+	if (fs.existsSync('temp/main.txt')) {
+		fs.readFile('temp/main.txt', 'utf-8', function (err, data) {
+		    if (err) {
+		    	throw err;
+		    }else{
+		    	// on suppose que le .txt est la forme X,Y,....
+				var params = data.split(',');
+				res.json({'params': params});
+		    }
+		});
+	}else{
+		var error = 'temp/main.txt not found';
+		res.json({'error': error});
+	}
+});
+
+
 
 module.exports = router;
 
