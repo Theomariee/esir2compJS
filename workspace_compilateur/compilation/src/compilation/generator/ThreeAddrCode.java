646fffbd40ccd9765f84e33194bb61467f3f1d32
Nicolas <nico35490@gmail.com>
2018-12-10 00:24:56 +0100

    whlib.

--- workspace_compilateur/compilation/src/compilation/generator/ThreeAddrCode.java
+++ workspace_compilateur/compilation/src/compilation/generator/ThreeAddrCode.java
@@ -21,7 +21,7 @@ public class ThreeAddrCode {
 	public String compile() {
 		switch(this.op) {
 		case "nop" :
-			return "nop();";
+			return "whlib.nop();";
 		case "ret" :
 			return "return "+addr1+";";
 		case "push":
@@ -31,26 +31,26 @@ public class ThreeAddrCode {
 		case "aff":
 			return addr1+" = "+addr2+";";
 		case "nil":
-			return addr1+" = nil();";
+			return addr1+" = whlib.nil();";
 		case "cons":
-			return addr1+" = cons("+addr2+", "+addr3+");"; // return concaténation de addr2(gauche) et addr3(droite) 
+			return addr1+" = whlib.cons("+addr2+", "+addr3+");"; // return concaténation de addr2(gauche) et addr3(droite) 
 			// Attention à bien différentier si addr3 est un bintree ou pas. Si oui, merger, sinon, retourner addr2 sans concaténation (ca veut dire que c'est l'init)!
 		case "hd":
-			return addr1+" = hd("+addr2+");";
+			return addr1+" = whlib.hd("+addr2+");";
 		case "tl":
-			return addr1+" = tl("+addr2+");";
+			return addr1+" = whlib.tl("+addr2+");";
 		case "!":
-			return addr1+" = not("+addr2+");";
+			return addr1+" = whlib.not("+addr2+");";
 		case "and":
-			return addr1+" = and("+addr2+","+addr3+");";
+			return addr1+" = whlib.and("+addr2+","+addr3+");";
 		case "or":
-			return addr1+" = or("+addr2+","+addr3+");";
+			return addr1+" = whlib.or("+addr2+","+addr3+");";
 		case "=?":
-			return addr1+" = eq("+addr2+","+addr3+");";
+			return addr1+" = whlib.eq("+addr2+","+addr3+");";
 		case "symb":
-			return addr1+" = symb(\'"+addr2+"\');";
+			return addr1+" = whlib.symb(\'"+addr2+"\');";
 		default :
-			return "nonImpl();";
+			return "whlib.nonImpl();";
 		}
 	}
 	
